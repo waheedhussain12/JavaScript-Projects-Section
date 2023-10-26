2989892489491bc3cc1f4978022f249a8dd27e45
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-26 11:07:50 +0500

    done

--- Temperature-Converter/temp.js
+++ Temperature-Converter/temp.js
@@ -7,6 +7,7 @@ cel.addEventListener("input",function(){
         f = f.toFixed(4)
     }
     fah.value = f;
+    // console.log(f);
 });
 fah.addEventListener("input",function(){
    let f = this.value;
