7e4d390cd27466403fbbd2669e3ea3b9cedbf761
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-27 11:40:16 +0500

    done

--- Temperature-Converter/temp.js
+++ Temperature-Converter/temp.js
@@ -1,5 +1,9 @@
 let cel = document.getElementById("cel");
 let fah = document.getElementById("fah");
+
+
+
+
 cel.addEventListener("input",function(){
     let c   = cel.value;
     let f = (c * 9/5) + 32;
@@ -13,6 +17,9 @@ cel.addEventListener("input",function(){
     // document.body.appendChild(newAdd);
     fah.value = f;
     console.log(f);
+
+    let rt = document.getElementById("result1");
+    result1.innerHTML =`Celsius To Fahrenheit ${fah.value = f} `;
 });
 
 
@@ -28,5 +35,18 @@ fah.addEventListener("input",function(){
  if(!Number.isInteger(c)){
     c = c.toFixed(4)
 }
+let rt = document.getElementById("result1");
+result1.innerHTML =`Fahrenheit To Celsius ${cel.value = c} `;
  cel.value = c;
 });
+
+
+
+
+function converter(){
+    let temp = document.getElementById("input").value;
+    let result  = (temp*1.8+32);
+    console.log(result);
+    let output = document.getElementById("result");
+    output.innerHTML += result
+}
