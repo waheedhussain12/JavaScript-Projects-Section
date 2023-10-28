7fe4e895b5e07c54d0bfa84070877179ba440594
Waheed Hussain <tommyshelby811@gmail.com>
2023-10-28 23:53:25 +0500

    done

---
+++ Words-Character-Counter/Words-Character-Counter.js
@@ -0,0 +1,26 @@
+let textbox = document.getElementById("textbox");
+textbox.addEventListener("input",function(){
+let userInputText = textbox.value;
+// console.log(getText.length);
+let char = userInputText.length;
+let characterCount = document.getElementById("char");
+characterCount.innerHTML = char;
+// document.getElementById("char").innerHTML = char;
+
+
+
+// words
+userInputText =  userInputText.trim();
+let words = userInputText.split(" ");
+let cleanList = words.filter(function(e){
+
+    return e != "";
+});
+console.log(cleanList);
+// let wordsLength = words.length;
+// let wordCounter = document.getElementById("word");
+// wordCounter.innerHTML = wordsLength;
+document.getElementById("word").innerHTML = cleanList.length;
+
+console.log();
+});
