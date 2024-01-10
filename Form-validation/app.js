1aa3f13a54afe8c037e5bf4b08676b86b9938ff8
waheedhussain <tommyshelby811@gmail.com>
2024-01-11 01:51:07 +0500

    done

--- Form-validation/app.js
+++ Form-validation/app.js
@@ -1,8 +1,7 @@
 let username = document.getElementById("username");
 let password = document.getElementById("password");
 function validateForm(){
-
-
-    
+// means form not submited
+// return false;
 }
 
