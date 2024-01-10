62f1369aeb2f3d929ede57e09068de48e46a45be
waheedhussain <tommyshelby811@gmail.com>
2024-01-11 02:10:20 +0500

    done

--- Form-validation/app.js
+++ Form-validation/app.js
@@ -1,16 +1,32 @@
 let username = document.getElementById("username");
 let password = document.getElementById("password");
-function validateForm(){
-// means form not submited
-// return false;
-if(username.value == ""){
-    document.getElementById("userError").innerHTML = "User Name is empty";
-     
+let flag = 1;
 
-}else if(username.value.length < 3){
-    
+function validateForm() {
+  // means form not submited
+  // return false;
+  if (username.value == "") {
+    document.getElementById("userError").innerHTML = "UserName is empty";
+    flag = 0;
+  } else if (username.value.length < 3) {
+    document.getElementById("userError").innerHTML =
+      "UserName require min 3 char";
+    flag = 0;
+  } else {
+    document.getElementById("userError").innerHTML = "";
+    flag = 1;
+  }
+  if (password.value == "") {
+    document.getElementById("passError").innerHTML = "Password is empty";
 
+    flag = 0;
+  } else {
+    document.getElementById("passError").innerHTML = "";
+    flag = 1;
+  }
+  if (flag) {
+    return true;
+  } else {
+    return false;
+  }
 }
-return false
-}
-
